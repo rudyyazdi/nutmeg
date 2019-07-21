b99274f34b736da32038cb575368c88469cbd20c
rudyyazdi <rudy@hireup.com.au>
2019-07-21 12:30:10 +1000

    new router

--- lib/message/application.ex
+++ lib/message/application.ex
@@ -2,6 +2,7 @@ defmodule Message.Application do
   use Application
   require Logger
 
+  @spec start(any, any) :: {:error, any} | {:ok, pid}
   def start(_type, _args) do
     children = [
       {Plug.Cowboy, scheme: :http, plug: Message.Router, options: [port: 8080]}
--- lib/message/router.ex
+++ lib/message/router.ex
@@ -1,11 +1,37 @@
 defmodule Message.Router do
-  import Plug.Conn
+  # import Plug.Conn
 
-  def init(options), do: options
+  # @spec init(any) :: any
+  # def init(options), do: options
 
-  def call(conn, _opts) do
-    conn
-    |> put_resp_content_type("text/plain")
-    |> send_resp(200, "Hello World3!\n")
+  # @spec call(Plug.Conn.t(), any) :: Plug.Conn.t()
+  # def call(conn, _opts) do
+  #   conn
+  #   IO.inspect(conn)
+
+  #   |> put_resp_content_type("text/plain")
+  #   |> send_resp(200, "Hello World3!\n")
+  # end
+
+
+  use Plug.Router
+
+  # This module is a Plug, that also implements it's own plug pipeline, below:
+
+  # Using Plug.Logger for logging request information
+  plug(Plug.Logger)
+  # Import functions used for matching routes
+  plug(:match)
+  # Import functions used for dispatching responses
+  plug(:dispatch)
+
+  # A simple route to test that the server is up
+  # Note, all routes must return a connection as per the Plug spec.
+  get "/" do
+    send_resp(conn, 200, "Hello World!\n")
+  end
+
+  match _ do
+    send_resp(conn, 404, "404")
   end
 end
