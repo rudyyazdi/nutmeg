f5269e9cd89ad852de61a6877e655defef2cbf2f
rudyyazdi <rudy@hireup.com.au>
2019-07-21 13:35:19 +1000

    send json

--- lib/message/router.ex
+++ lib/message/router.ex
@@ -22,13 +22,17 @@ defmodule Message.Router do
   plug(Plug.Logger)
   # Import functions used for matching routes
   plug(:match)
+  # Using Poison for JSON decoding
+  # Note, order of plugs is important, by placing this _after_ the 'match' plug,
+  # we will only parse the request AFTER there is a route match.
+  plug(Plug.Parsers, parsers: [:json], json_decoder: Poison)
   # Import functions used for dispatching responses
   plug(:dispatch)
 
   # A simple route to test that the server is up
   # Note, all routes must return a connection as per the Plug spec.
   get "/" do
-    send_resp(conn, 200, "Hello World!\n")
+    send_resp(conn, 200, Poison.encode!(%{"status" => "ok"}))
   end
 
   match _ do
